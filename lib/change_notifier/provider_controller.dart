b73cd2c81ce689b261e6a02816d5e656c4b19be0
Enzo D <enzo.danjour@gmail.com>
2021-12-22 17:55:43 -0300

    add to debug in dart dev tools

--- lib/change_notifier/provider_controller.dart
+++ lib/change_notifier/provider_controller.dart
@@ -1,7 +1,7 @@
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-class ProviderController extends ChangeNotifier{
+class ProviderController extends ChangeNotifier with DiagnosticableTreeMixin{
   String name = 'Nome';
   String imgAvatar = 'https://i.imgur.com/k0AYd9D.jpg';
   String birthDate='data';
@@ -15,5 +15,15 @@ class ProviderController extends ChangeNotifier{
   void alterarNome(){
     name = 'Enzo Danjour';
     notifyListeners();
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    // list all the properties of your class here.
+    // See the documentation of debugFillProperties for more information.
+    properties.add(StringProperty('name', name));
+    properties.add(StringProperty('imgAvatar', imgAvatar));
+    properties.add(StringProperty('birthDate', birthDate));
   }
 }
